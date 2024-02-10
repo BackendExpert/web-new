3b1f33488118af506cf9e7c56da425b6cf8404dd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-10 20:16:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/researchSub.tsx
+++ src/research/researchSub.tsx
@@ -3,6 +3,7 @@ const researchSub = () => {
   return (
     <div className="container mx-auto px-12">
         <h1 className="">Research Division at NIFS</h1>
+        <p className="">Advancing tge frontiers fundamental</p>
     </div>
   )
 }

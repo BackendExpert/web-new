109b7cf0c87054b4674c3658323166a209f7ac70
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-10 20:27:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/researchSub.tsx
+++ src/research/researchSub.tsx
@@ -5,8 +5,8 @@ const researchSub = () => {
         <h1 className="">Research Division at NIFS</h1>
         <p className="">Advancing the frontiers of fundamental research</p>
 
-        <div className="">
-            
+        <div className="md:grid grid-cols-3 gap-4">
+    
         </div>
     </div>
   )

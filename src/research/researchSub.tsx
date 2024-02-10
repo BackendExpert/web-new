c63f782f861bc246c5b4825626035d3bccc238cb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-10 20:33:51 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/researchSub.tsx
+++ src/research/researchSub.tsx
@@ -3,9 +3,10 @@ import IonIcon from "@reacticons/ionicons"
 const researchSub = () => {
   return (
     <div className="container mx-auto px-12">
-        <h1 className="">Research Division at NIFS</h1>
-        <p className="">Advancing the frontiers of fundamental research</p>
-
+        <div className="">
+            <h1 className="">Research Division at NIFS</h1>
+            <p className="">Advancing the frontiers of fundamental research</p>
+        </div>
         <div className="md:grid grid-cols-3 gap-4">
             <div className="">
                 Biological Science

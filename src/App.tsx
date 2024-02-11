3423534684f334be502531aecefa7ae0ef1d27c8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:03:46 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ResearchSub from "./research/researchSub";
 import Bio from "./research/BIo";
+import EarthSpace from "./research/EarthSpace";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/Research" element={<ResearchSub />}></Route>
         <Route path="/Research/Bio" element={<Bio />} ></Route>
+        <Route path="/Research/EarthSpace" element={<EarthSpace />}></Route>
       </Routes>
     </BrowserRouter>
   )

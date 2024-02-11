2f3c47dd6384c24acd0f68ddb2cbc175735c0fe9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 19:25:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -1,16 +1,19 @@
+import Footer from "../footer/Footer"
+import NavBar from "../nav/Navbar"
 
 const BIo = () => {
   return (
     <div className="">
-        <div className="md:grid grid-cols-2 gap-0">
-            <div className="">
-                <div className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp2237889.jpg')]"></div>
-                <img src="https://wallpapercave.com/wp/wp2237889.jpg" alt="" className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp2237889.jpg')]" />
+        <NavBar />
+            <div className="md:grid grid-cols-2 gap-0">
+                <div className="">
+                    <div className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp2237889.jpg')] h-[60vh] bg-cover" ></div>
+                </div>
+                <div className="bg-gray-800 md:">
+                
+                </div>            
             </div>
-            <div className="bg-gray-800 md:">
-            
-            </div>            
-        </div>
+        <Footer />
     </div>
   )
 }

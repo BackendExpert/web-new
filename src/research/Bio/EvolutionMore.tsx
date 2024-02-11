3988975e71737d7699992091d8ef7f66de73ad32
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 03:08:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/Bio/EvolutionMore.tsx
+++ src/research/Bio/EvolutionMore.tsx
@@ -2,6 +2,7 @@ const EvolutionMore = () => {
   return (
     <div className="container mx-auto px-10 my-16">
         <p className="text-3xl font-semibold">Evolution of the Jumping Spaiders (Salticidae)</p>
+        <img src="https://wallpapercave.com/wp/wp7064156.jpg" alt=""  className="rounded "/>
     </div>
   )
 }

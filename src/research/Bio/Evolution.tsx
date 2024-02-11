cc4862fe639f6c2fc5bd2285db1d7130de14f319
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 02:10:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/Bio/Evolution.tsx
+++ src/research/Bio/Evolution.tsx
@@ -2,7 +2,7 @@
 const Evolution = () => {
   return (
     <div className="container mx-auto px-10 my-16">
-      <p className="text-2xl font-semibold">Evolution, Ecology and Biodiversity Research Program</p>  
+      <p className="text-3xl font-semibold">Evolution, Ecology and Biodiversity Research Program</p>  
     </div>
   )
 }

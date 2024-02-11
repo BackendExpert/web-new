a3d9220244628dc7ccaae20317ba93ac81078680
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 21:21:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -8,7 +8,8 @@ import { Link } from "react-router-dom"
 const BIo = () => {
   const [collspanOpen, SetCollspan] = useState(false);
   const [foodOpen, SetFood] = useState(false);
-  
+  const [Microbio, SetMicroBio] = useState(false);
+
   return (
     <div className="">
         <NavBar />

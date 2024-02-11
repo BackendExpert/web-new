ec2cdafc16ba869ceb65edab9f9d7ce3217a8993
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 20:28:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -10,11 +10,15 @@ const BIo = () => {
                     <div className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp2237889.jpg')] h-[60vh] bg-cover" ></div>
                 </div>
                 <div className="bg-gray-800 md:visible invisible">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas atque quaerat expedita doloremque natus iusto quis amet. Dolor, asperiores? Earum, nulla doloremque ab dolore reprehenderit id necessitatibus libero eos quidem.
                 </div>                     
             </div>
             <div className="flex">
               <span className="text-white text-4xl font-semibold md:mt-[-300px] md:ml-[55%] mt-[-300px] ml-[10%]">Biological Science</span>
+            </div>
+
+            <div className="">
+              <p className="">Background</p>
+              <p className="">The main focus of research in this division is discovering, eveluating and developing the island's bountiful biofic and abiotic natural reseources. Research activties are also focussed on efficient use of existing reseources and maintaining a cleaner enviroment</p>
             </div>
         <Footer />
     </div>

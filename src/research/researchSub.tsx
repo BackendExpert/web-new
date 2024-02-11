d566af23e33ea359fc8977095fe09dfead270052
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:12:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/researchSub.tsx
+++ src/research/researchSub.tsx
@@ -76,7 +76,7 @@ const researchSub = () => {
                 </Link>             
             </div>
             <div className="">
-                <Link>
+                <Link to={'/Research/SocialSci'}>
                     <div className="rounded bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp13134266.jpg')] h-72 bg-cover bg-center bg-no-repeat mb-8 bg-opacity-75" >
                         <div className="px-8 py-28">
                             <span className="font-bold text-2xl text-white">Philosophy and Social Science</span>  

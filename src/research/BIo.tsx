96f291726e6da54d5bbfd8a3aed8838d7e03c178
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:32:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -3,8 +3,10 @@ const BIo = () => {
   return (
     <div className="">
         <div className="md:grid grid-cols-2 gap-0">
-            <div className="">Biological Science</div>
-            Image
+            <div className=""></div>
+            <div className="">
+            
+            </div>            
         </div>
     </div>
   )

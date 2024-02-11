ae69435e5da2a9798ab18ad7e49893f5bbf431fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 03:06:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/Bio/EvolutionMore.tsx
+++ src/research/Bio/EvolutionMore.tsx
@@ -1,6 +1,8 @@
 const EvolutionMore = () => {
   return (
-    <div>EvolutionMore</div>
+    <div className="container mx-auto px-10 my-16">
+        <p className="text-3xl font-semibold">Evolution of the Jumping Spaiders (Salticidae)</p>
+    </div>
   )
 }
 

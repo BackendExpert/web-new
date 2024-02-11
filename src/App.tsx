0d6927a6ea92465362853f3ec83c9cb848f8106b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:11:45 +0530

    Updae
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Bio from "./research/BIo";
 import EarthSpace from "./research/EarthSpace";
 import ComputerMaths from "./research/ComputerMaths";
 import Env from "./research/Env";
+import SocialSci from "./research/SocialSci";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/Research/EarthSpace" element={<EarthSpace />}></Route>
         <Route path="/Research/ComputerMaths" element={<ComputerMaths />}></Route>
         <Route path="/Research/Env" element={<Env />}></Route>
+        <Route path="/Research/SocialSci" element={<SocialSci />}></Route>
       </Routes>
     </BrowserRouter>
   )

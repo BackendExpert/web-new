ba19c02d4ca2163b632ae442f3175be6cfeb0187
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:35:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -4,9 +4,9 @@ const BIo = () => {
     <div className="">
         <div className="md:grid grid-cols-2 gap-0">
             <div className="">
-                <img src="https://wallpapercave.com/wp/wp2237889.jpg" alt="" className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8)),url('https://wallpapercave.com/wp/wp2237889.jpg')]" />
+                <img src="https://wallpapercave.com/wp/wp2237889.jpg" alt="" className="bg-[linear-gradient(to_right_bottom,rgba(190,190,190,0.8),rgba(0,0,0,0.8))]" />
             </div>
-            <div className="bg-gray-800 md:">
+            <div className="bg-gray-800 md:visible invisible">
             
             </div>            
         </div>

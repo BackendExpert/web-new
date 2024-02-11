ca583b58b626314ff8bb3d573c7572efe8a552cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 06:33:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -3,8 +3,10 @@ const BIo = () => {
   return (
     <div className="">
         <div className="md:grid grid-cols-2 gap-0">
-            <div className=""></div>
             <div className="">
+                <img src="https://wallpapercave.com/wp/wp2237889.jpg" alt="" className="" />
+            </div>
+            <div className="bg-gray-800 md:">
             
             </div>            
         </div>

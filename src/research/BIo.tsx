e6285785ddc043bbc7aa08e1f89c6d06dbe8e003
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-11 20:50:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/BIo.tsx
+++ src/research/BIo.tsx
@@ -42,9 +42,31 @@ const BIo = () => {
               </div>
               <div className="pt-4 flex">
                 <p><IonIcon name="call" size="large"></IonIcon></p>
-                <p className="pl-4 pt-[3px]">(+94) 81 22 32 106, (+94) 81 22 32 107</p>
-                
+                <p className="pl-4 pt-[3px]">(+94) 81 22 32 106, (+94) 81 22 32 107</p>                
               </div>
+
+              <div className="bg-white text-xl px-12 py-8 rounded">
+                <div className="flex justify-between w-full font-semibold" onClick={() => SetCollspan(!collspanOpen)}>
+                  <p className="">This is title 1</p>
+                  <p className="pt-[3px]">
+                    <IonIcon name={collspanOpen ? "chevron-up-circle-outline" : "chevron-down-circle-outline"} />
+                  </p>
+
+                </div>
+                  <div className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+                    collspanOpen ? 'grid-rows-[1fr] opacity-100 pt-4' : 'grid-rows-[0fr] opacity-0'
+                  }`}>          
+                    <div className="overflow-hidden">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur nemo soluta eum iste itaque iusto, natus odio incidunt quibusdam ratione deleniti consequuntur, omnis non, delectus enim vitae velit magni maiores.
+                      Aspernatur in omnis aut rem accusamus quibusdam repellat excepturi beatae id minima, incidunt, voluptates eaque nesciunt, nulla vel ducimus modi? Iste rem sint dignissimos praesentium provident modi veritatis, pariatur perferendis?
+                      Hic ipsam voluptatum ullam molestias. Doloremque cumque, nisi, id unde temporibus odio sed minus, molestiae quasi aperiam dicta. Magni reiciendis vero soluta, blanditiis tenetur ipsam facilis quidem commodi eum deleniti?
+                      Modi tempore alias sunt nam, assumenda saepe numquam minus, repellendus culpa provident ab exercitationem fuga, quo at maiores. Reprehenderit beatae repudiandae autem voluptatibus maxime aperiam quae totam corrupti eos praesentium.
+                      Sunt necessitatibus omnis, provident dolorum molestiae, non cum laudantium officia adipisci tempora, natus eum? Culpa optio est aspernatur sed, corrupti quo corporis blanditiis odit, voluptas, esse aut voluptate adipisci perspiciatis!
+                      Eaque vitae exercitationem tempore a quas totam quaerat enim dicta nostrum soluta beatae repellendus, nulla, commodi rerum? Quibusdam consectetur cupiditate officiis, soluta odit exercitationem accusamus modi facilis asperiores nostrum fugiat?  
+                    </div>    
+                  </div>
+              </div>
+
             </div>
         <Footer />
     </div>

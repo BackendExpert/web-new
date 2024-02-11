b33f390a8318aac459c087e9491117114268faa7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 02:11:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/Bio/Evolution.tsx
+++ src/research/Bio/Evolution.tsx
@@ -1,8 +1,15 @@
+import NavBar from "../../nav/Navbar"
+import Footer from "../../footer/Footer"
 
 const Evolution = () => {
   return (
-    <div className="container mx-auto px-10 my-16">
-      <p className="text-3xl font-semibold">Evolution, Ecology and Biodiversity Research Program</p>  
+    <div className="">
+      <NavBar />
+      <div className="container mx-auto px-10 my-16">
+        <p className="text-3xl font-semibold">Evolution, Ecology and Biodiversity Research Program</p>  
+      </div>
+
+      <Footer />
     </div>
   )
 }

9cf834c02164c6bc2e15241de7c1ede383dc9edf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 02:12:44 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/research/Bio/Evolution.tsx
+++ src/research/Bio/Evolution.tsx
@@ -7,6 +7,18 @@ const Evolution = () => {
       <NavBar />
       <div className="container mx-auto px-10 my-16">
         <p className="text-3xl font-semibold">Evolution, Ecology and Biodiversity Research Program</p>  
+        <p className="py-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, ex! Sit commodi alias, architecto explicabo, distinctio enim, natus voluptate quaerat neque quidem sequi error odio hic perspiciatis sint eius fugit.
+          Pariatur a corporis voluptatum alias, hic ipsa aliquid assumenda minus repellat. Laudantium mollitia cumque at! Dicta assumenda esse ipsum, mollitia fugit laudantium nam earum fugiat, similique, sunt eius natus amet?
+          Cumque, velit aliquid eius optio similique neque consequatur id debitis molestias quam? Veniam error aperiam corporis maiores fugiat beatae asperiores voluptatibus odit. Voluptate eum expedita laboriosam, quod reiciendis temporibus sed.
+          Molestiae vel fugiat explicabo aliquid blanditiis illo repellat itaque natus aut incidunt voluptatem at deleniti qui, dolor asperiores sit iste maxime similique voluptatum dolorem magni minima inventore ipsum. Voluptate, sequi.
+          Accusamus ipsa ratione voluptates impedit facere debitis animi, nisi blanditiis repellendus autem. Perspiciatis id officia dicta est, deleniti dolores consectetur sed consequuntur maiores, possimus, sit neque nulla eveniet ab ea.
+          Minima temporibus ipsum qui exercitationem, culpa illo. Culpa, quis voluptates similique quaerat dolorum omnis assumenda sunt nihil reprehenderit doloremque nesciunt voluptate. Adipisci iusto animi numquam debitis, nisi saepe aut at!
+          Quaerat nemo, iusto qui neque accusamus temporibus repellat at, suscipit nihil enim placeat eveniet dicta minus modi doloremque soluta sint. Quos nobis porro dignissimos molestias quas, voluptatum laborum corrupti reiciendis?
+          Asperiores quibusdam ut, fugiat porro corrupti ipsa illum iure est corporis quaerat atque cum delectus vitae natus assumenda distinctio sit hic at veniam nemo autem labore minus. Sapiente, incidunt provident?
+          Doloremque itaque ducimus inventore in maxime veritatis aliquam blanditiis adipisci animi fugiat distinctio aperiam, officia, voluptas ipsum, cumque culpa consequatur deleniti rem quasi consectetur voluptatibus delectus. Impedit velit distinctio nesciunt?
+          Eveniet officia deserunt nihil velit aliquam magnam culpa, suscipit reiciendis vero deleniti eum necessitatibus totam ipsam rerum possimus, eligendi enim voluptatum non. Dolores, distinctio id qui error in reiciendis commodi!
+        </p>
       </div>
 
       <Footer />
